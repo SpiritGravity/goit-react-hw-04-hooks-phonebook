c36a59306f6f16aba720b1a1050e4a2c0efbf8df
Anastasiia Tyshkevych <anastasia.tyshkevych@gmail.com>
2022-06-01 17:19:25 +0300

    summary-update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ContactForm from './ContactForm/contactForm';
 import ContactList from './ContactList/contactList';
 import Filter from './Filter/filter';
-// import useLocalStorage from './Hooks/useLocalStorage';
+import useLocalStorage from './Hooks/useLocalStorage';
 import {Container, Title, Subtitle} from './App.styled';
 import { nanoid } from 'nanoid';
 
-const contactDefault = [
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-];
+
 
 export default function App() {
 
-  const itemsContact = () => {
-    return  contactDefault;
-
-    // JSON.parse(localStorage.getItem('contacts')) || 
-  };
-
-  const [contacts, setContacts] = useState(itemsContact());
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
   const [filter, setFilter] = useState('');
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   const addContacts = ({ name, number }) => {
     const contact = {
@@ -44,10 +29,9 @@ export default function App() {
     setFilter(e.currentTarget.value);
   };
 
-  const getVisibleContacts = () => {
-    const normalizedFilter = filter.toLowerCase();
+  const getContacts = () => {
     return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
@@ -55,6 +39,7 @@ export default function App() {
     setContacts(contacts.filter(contact => contact.id !== contactId));
     setFilter('');
   };
+  
   return (
  <Container>
  <Title>Phonebook</Title>
@@ -67,7 +52,7 @@ export default function App() {
       onChange={changeFilter} 
       />
  <ContactList
-      contacts={getVisibleContacts()}
+      contacts={getContacts()}
       onDeleteContact={deleteContact}
         />
 </Container>
